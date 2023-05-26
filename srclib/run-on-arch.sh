0fc2022d7fc22a2850b9ec0b1aba7b7d6b991bad
yashratawa <72684548+yashratawa@users.noreply.github.com>
2023-05-26 17:53:39 +0530

    Update run-on-arch.sh

--- srclib/run-on-arch.sh
+++ srclib/run-on-arch.sh
@@ -3,8 +3,8 @@
 set -euo pipefail
 
 # Args
-DOCKERFILE=$1
-CONTAINER_NAME=$2
+DOCKERFILE=Dockerfile.s390x.ubuntu20.04
+CONTAINER_NAME=c1
 # Remainder of args get passed to docke
 declare -a DOCKER_RUN_ARGS=${@:3:${#@}}
 
