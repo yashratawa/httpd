0ca3a3ffafd602879f20accac5d9c4e76e5a6608
yashratawa <72684548+yashratawa@users.noreply.github.com>
2023-05-26 20:01:12 +0530

    Update run-on-arch.sh

--- srclib/run-on-arch.sh
+++ srclib/run-on-arch.sh
@@ -37,8 +37,8 @@ install_deps () {
   #            linux/386, linux/arm/v7, linux/arm/v6
   apt-get update -q -y
   apt-get -qq install -y qemu qemu-user-static
-  #apt-get install docker
-  docker pull multiarch/qemu-user-static
+  apt-get install docker
+  #docker pull multiarch/qemu-user-static
   docker run --rm --privileged multiarch/qemu-user-static --reset -p yes --credential yes
 }
 
