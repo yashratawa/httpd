c5f6236e824e53da15fc3cff2a269680f7803755
yashratawa <72684548+yashratawa@users.noreply.github.com>
2023-05-29 15:12:27 +0530

    Update run-on-arch.sh

--- srclib/run-on-arch.sh
+++ srclib/run-on-arch.sh
@@ -39,8 +39,7 @@ install_deps () {
   apt-get -qq install -y qemu qemu-user-static
   apt-get install docker
   #docker pull multiarch/qemu-user-static
-  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
-  #--credential yes
+  docker run --rm --privileged multiarch/qemu-user-static --reset -p yes --credential yes
 }
 
 build_container () {
