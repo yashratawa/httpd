b3c3471bf9029446423fc33d4cb8ba50b136c282
yashratawa <72684548+yashratawa@users.noreply.github.com>
2023-05-29 17:41:49 +0530

    Update run-on-arch.sh

--- srclib/run-on-arch.sh
+++ srclib/run-on-arch.sh
@@ -38,19 +38,19 @@ install_deps () {
   #            linux/386, linux/arm/v7, linux/arm/v6
   apt-get update -q -y
   apt-get -qq install -y qemu qemu-user-static
-  #apt-get install docker -y
+  apt-get install docker.io -y
   #sudo docker pull multiarch/qemu-user-static
   #apt update
 
-  apt install apt-transport-https ca-certificates curl software-properties-common -y
+#   apt install apt-transport-https ca-certificates curl software-properties-common -y
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 
-  add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#   add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 
-  apt-cache policy docker-ce
+#   apt-cache policy docker-ce
 
-  apt install docker-ce -y
+#   apt install docker-ce -y
 
 
 
