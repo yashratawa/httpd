08657a6638895e17e7116843e4a3f280ff0c1bef
yashratawa <72684548+yashratawa@users.noreply.github.com>
2023-05-30 19:27:31 +0530

    Update travis_run_linux.sh

--- test/travis_run_linux.sh
+++ test/travis_run_linux.sh
@@ -29,7 +29,7 @@ fi
 ### Debian packages helpfully install them, so use the system APR to buildconf
 #apt install libapr1 libaprutil1 -y
 apt install apr -y
-find / -name apr
+export PATH="/usr/bin:$PATH"
 ./buildconf --with-apr=/usr/bin/apr-1-config ${BUILDCONFIG}
 
 PREFIX=${PREFIX:-$HOME/build/httpd-root}
