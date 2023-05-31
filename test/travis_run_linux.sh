e645dfce6cee9767fb584fa35261593faa11b74d
yashratawa <72684548+yashratawa@users.noreply.github.com>
2023-05-31 11:17:43 +0530

    Update travis_run_linux.sh

--- test/travis_run_linux.sh
+++ test/travis_run_linux.sh
@@ -27,8 +27,8 @@ fi
 
 ### Installed apr/apr-util don't include the *.m4 files but the
 ### Debian packages helpfully install them, so use the system APR to buildconf
-#apt install libapr1 libaprutil1 -y
-apt install apr -y
+apt install libapr1 libaprutil1 -y
+#apt install apr -y
 exit 0
 #export PATH="/usr/bin:$PATH"
 ./buildconf --with-apr=/usr/bin/apr-1-config ${BUILDCONFIG}
