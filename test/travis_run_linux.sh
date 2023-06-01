d24550d85b9b6bd5f38bbde312a0d72829632487
yashratawa <72684548+yashratawa@users.noreply.github.com>
2023-06-01 18:00:41 +0530

    Update travis_run_linux.sh

--- test/travis_run_linux.sh
+++ test/travis_run_linux.sh
@@ -47,7 +47,7 @@ if test ! -v SKIP_TESTING; then
     apt install curl -y
     apt install cpanminus -y
     apt install libperlio-gzip-perl
-    curl -L https://cpanmin.us | perl - --sudo App::cpanminus
+    curl -L https://cpanmin.us | perl - --sudo App::cpanminus --force
     #cpanm PerlIO -y
     #perl - --sudo App::cpanminus
     cpan local::lib
