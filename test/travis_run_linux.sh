48ffc423584db4d24e5bab2b0ed80093afa28922
yashratawa <72684548+yashratawa@users.noreply.github.com>
2023-06-01 16:30:09 +0530

    Update travis_run_linux.sh

--- test/travis_run_linux.sh
+++ test/travis_run_linux.sh
@@ -44,8 +44,9 @@ if test ! -v SKIP_TESTING; then
 
     # Use the CPAN environment.
     apt install curl -y
-    curl -L https://cpanmin.us | perl - --sudo App::cpanminus
+    curl -L https://cpanmin.us
     cpan PerlIO
+    perl - --sudo App::cpanminus
     cpan local::lib
     eval $(perl -I ~/perl5/lib/perl5/ -Mlocal::lib)
 fi
